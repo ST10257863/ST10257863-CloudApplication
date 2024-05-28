2b21d87be8c5f9be11d0087c86194faf9236dbef
Jamie Parker <129764378+ST10257863@users.noreply.github.com>
2024-05-28 13:24:46 +0200

    Ensuring Main is Up to Date

--- obj/Debug/net8.0/CloudApplication.AssemblyInfo.cs
+++ obj/Debug/net8.0/CloudApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CloudApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18f923dc83c3bb49f7b1d51db40260c0ef3de95d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee307e16e8314d3d4ccb2f4b5ebe5b7f4fef5e50")]
 [assembly: System.Reflection.AssemblyProductAttribute("CloudApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CloudApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
