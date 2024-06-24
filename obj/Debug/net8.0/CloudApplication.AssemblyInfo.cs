8d55628ec71ec3cf8fdbdb799569a95ef24cfe96
Jamie Parker <129764378+ST10257863@users.noreply.github.com>
2024-06-24 17:54:13 +0200

    Azure

--- obj/Debug/net8.0/CloudApplication.AssemblyInfo.cs
+++ obj/Debug/net8.0/CloudApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CloudApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05534894db0d5cb4837e49533a5c84c4f7870f60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afefde6a8d2598429c828924a17c422610fad3e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CloudApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CloudApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
